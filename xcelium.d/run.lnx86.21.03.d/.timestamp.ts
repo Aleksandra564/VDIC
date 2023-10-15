585efeba92d2976f68b5ff26bcdc802ee49ef7f1
Aleksandra564 <fudali.aleksandra01@gmail.com>
2023-10-15 12:42:20 +0200

    lab1

---
+++ xcelium.d/run.lnx86.21.03.d/.timestamp.ts
@@ -0,0 +1,4 @@
+1696516184 /home/student/afudali/Desktop/VDIC/common/tinyalu/tinyalu.sv
+1697365724 /home/student/afudali/Desktop/VDIC/lab01example/tb/tinyalu_tb.sv
+1696514400 /home/student/afudali/Desktop/VDIC/common/tinyalu/single_cycle.sv
+1696515437 /home/student/afudali/Desktop/VDIC/common/tinyalu/three_cycle.sv
