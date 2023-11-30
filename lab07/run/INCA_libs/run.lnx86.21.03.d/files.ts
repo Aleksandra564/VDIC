8c8312dd477ce8824836d665b6b18e4364643b49
Aleksandra564 <fudali.aleksandra01@gmail.com>
2023-11-30 08:26:25 +0100

    initial commit

---
+++ lab07/run/INCA_libs/run.lnx86.21.03.d/files.ts
@@ -0,0 +1,4 @@
+1701287444 /home/student/afudali/Desktop/VDIC/lab06/tb/mult_pkg.sv
+1701287793 /home/student/afudali/Desktop/VDIC/lab06/tb/mult_bfm.sv
+1697478212 /home/student/afudali/Desktop/VDIC/common/vdic_dut_2023.svp
+1700511122 /home/student/afudali/Desktop/VDIC/lab06/tb/top.sv
