2e2c8e28003705bd627fcc76b4f2ec5689d2b16e
Aleksandra564 <fudali.aleksandra01@gmail.com>
2023-12-05 21:21:34 +0100

    lab08 initial commit

---
+++ lab08/tb/tb_classes/random_test.svh
@@ -0,0 +1,42 @@
+class random_test extends uvm_test;
+    `uvm_component_utils(random_test)
+
+//------------------------------------------------------------------------------
+// local variables
+//------------------------------------------------------------------------------
+    env env_h;
+
+//------------------------------------------------------------------------------
+// constructor
+//------------------------------------------------------------------------------
+    function new (string name, uvm_component parent);
+        super.new(name,parent);
+    endfunction : new
+
+//------------------------------------------------------------------------------
+// build phase
+//------------------------------------------------------------------------------
+    function void build_phase(uvm_phase phase);
+        env_h = env::type_id::create("env_h",this);
+    endfunction : build_phase
+
+//------------------------------------------------------------------------------
+// end-of-elaboration phase
+//------------------------------------------------------------------------------
+    function void end_of_elaboration_phase(uvm_phase phase);
+        command_transaction tmp;	// transaction object to check the type generated
+
+        set_print_color(COLOR_BLUE_ON_WHITE);
+        this.print(uvm_default_table_printer);	// print test env topology
+        set_print_color(COLOR_DEFAULT);
+
+        // printing the type of the transaction generated
+        tmp = command_transaction::type_id::create("command_transaction", this);
+        set_print_color(COLOR_BOLD_BLACK_ON_YELLOW);
+        `uvm_info("COMMAND TRANSACTION", tmp.get_type_name(), UVM_NONE)
+        set_print_color(COLOR_DEFAULT);
+    endfunction : end_of_elaboration_phase
+
+endclass
+
+
---
+++ lab08/tb/tb_classes/result_monitor.svh
@@ -0,0 +1,53 @@
+class result_monitor extends uvm_component;
+    `uvm_component_utils(result_monitor)
+
+//------------------------------------------------------------------------------
+// local variables
+//------------------------------------------------------------------------------
+    protected virtual mult_bfm bfm;
+    uvm_analysis_port #(result_transaction) ap;
+
+//------------------------------------------------------------------------------
+// constructor
+//------------------------------------------------------------------------------
+    function new (string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction : new
+
+//------------------------------------------------------------------------------
+// build phase
+//------------------------------------------------------------------------------
+    function void build_phase(uvm_phase phase);
+        if(!uvm_config_db #(virtual mult_bfm)::get(null, "*", "bfm", bfm))
+            `uvm_fatal("RESULT MONITOR", "Failed to get BFM")
+
+        bfm.result_monitor_h = this;
+        ap = new("ap",this);
+    endfunction : build_phase
+
+//------------------------------------------------------------------------------
+// access function for BFM
+//------------------------------------------------------------------------------
+    function void write_to_monitor(
+		logic signed 	[31:0] 	result,
+		logic 					result_parity,
+		logic 					arg_parity_error
+	    );
+	    
+        result_transaction result_t;
+        result_t = new("result_t");
+	    
+	    result_t.result = result;
+	    result_t.result_parity = result_parity;
+	    result_t.arg_parity_error = arg_parity_error;
+	    
+        ap.write(result_t);
+    endfunction : write_to_monitor
+
+endclass : result_monitor
+
+
+
+
+
+
